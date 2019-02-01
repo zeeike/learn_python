64ab8b814f26417ab58ffd744f46b08bb6e8a0be
steve <steve@localhost.localdomain>
2019-02-01 20:59:06 +1100

    Started using variables

---
+++ variables.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+character_name = "John"
+character_age = "35"
+
+print("There once was a man named " + character_name + ",")
+print("he was " + character_age + " years old.")
+print("He really liked the name " + character_name + ",")
+print("but he didn't like being " + character_age + "!")
