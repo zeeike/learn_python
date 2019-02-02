df9bec7d48464e9fc2889d443e30909040d7294b
steve <steve@localhost.localdomain>
2019-02-02 13:21:27 +1100

    Numbers, strings and basic user input

---
+++ strings.py
@@ -0,0 +1,6 @@
+phrase = "This is a new string"
+print(phrase.upper().isupper())
+print(len(phrase))
+print(phrase[0])
+print(phrase.index("n"))
+print(phrase.replace("This", "Terrible"))
---
+++ user_input.py
@@ -0,0 +1,6 @@
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+print("Hello there " + name)
+print("You are " + age + " years old!")
